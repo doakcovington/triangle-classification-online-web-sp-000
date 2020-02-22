6fb23ae7f4cf7204f0df5435da89597482daa213
Doak Covington <doakcovington@protonmail.com>
2020-02-22 23:49:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,35 +2,7 @@ require 'pry'
 
 class Triangle
   # write code here
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(a,b,c)
+
   end
-
-  def kind
-    if @side_1 == @side_2 && @side_1 == @side_3
-      :equilateral
-    elsif @side_1 == @side_2 && @side_1 != @side_3
-      :isosceles
-    elsif @side_1!= @side_2 && @side_1 == @side_3
-      :isosceles
-    elsif @side_2 == @side_3
-      :isosceles
-    elsif @side_1 != @side_2 && @side_1 != @side_3
-      :scalene
-    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-      raise TriangleError
-    end#end of if
-  end#end of kind
-
-  def valid?
-    if @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-      raise TriangleError
-    end
-  end
-
-  class TriangleError < StandardError
-  end
-
 end
