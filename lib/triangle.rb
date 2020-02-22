516ea42d5830e7327020d8439299739a000db2ee
Doak Covington <doakcovington@protonmail.com>
2020-02-22 23:57:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   def kind
     if a == 0 || b == 0 || c == 0
       raise TriangleError
-    elsif a + b <= c || a + c <= b
+    elsif (a + b) <= c || (a + c) <= b || (c + b) <= a
       raise TriangleError
     end
   end
